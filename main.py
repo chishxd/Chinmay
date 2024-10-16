f305d56e2741301eb7d65cc79a87071166b55f0a
Chish <chishxd@gmail.com>
2024-10-16 18:44:49 +0530

    main.py

--- main.py
+++ main.py
@@ -1,45 +1,54 @@
 import random
 import time
 
+class Balance:
+    def __init__(self, balance = 10000):
+        self.balance = balance
+    def win_bal(self, amount):
+        self.balance += 2 * amount
+        return self.balance
+    def lose_bal(self, amount):
+        self.balance -= amount
+        return self.balance
+    def add_bal(self, amount):
+        self.balance += amount
+        return self.balance
 def setNum():
     return random.randint(1,10)
-def Accbalance(winner,i = 0,j = 0):
-    balance = 10000
-    if winner == 1:
-        balance += 2*i
-    if winner == 0:
-        balance -= i
-    if j > 0:
-        balance += i
-    return balance
-    
+
+balance = Balance()
 while True:
+    newbal = balance.balance
     answer = setNum()
+    
     print("Welcome to Ring! Fellow Warrior")
     time.sleep(1)
     bet = int(input("Place your Bet! The Number should be between 1 - 10 only! "))
-    time.sleep(2)
+    time.sleep(0)
     amt = int(input("How Much money are you willing to pay? "))
-    time.sleep(0.5)
-    print("Well said, Now")
-    time.sleep(1)
-    print("3")
-    time.sleep(1)
-    print("2")
-    time.sleep(1)
-    print("1")
-    time.sleep(1)
-    print(f"The Number is.... {answer}!!")
-    if bet == answer:
-        print("sattta jinklas ki bhava! Laav aani paishe")
-        balance = Accbalance(1,amt)
-        print(balance)
+    if amt < newbal:
+        time.sleep(0.5)
+        print("Well said, Now")
+        time.sleep(1)
+        print("3")
+        time.sleep(1)
+        print("2")
+        time.sleep(1)
+        print("1")
+        time.sleep(1)
+        print(f"The Number is.... {answer}!!")
+        if bet == answer:
+            print("sattta jinklas ki bhava! Laav aani paishe")
+            newbal = balance.win_bal(amt)
+        else:
+            print("Marda, kay aasa. Ya parat")
+            newbal = balance.lose_bal(amt)
+        if newbal == 0:
+            addbal = int(input("Bhava Paishe smplet ki... Ghaal aani: "))
+            newbal = balance.add_bal(addbal)
+        print(newbal)
     else:
-        print("Marda, kay aasa. Ya parat")
-        balance = Accbalance(0,0,amt)
-    if balance == 0:
-        addbal = int(input("Bhava Paishe smplet ki... Ghaal aani: "))
-    print(balance)
+        print("This Amount is not available on the account")
     e = input("Press E to exit: ").lower()
     if e == "e":
         break
