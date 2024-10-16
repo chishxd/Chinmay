4abcc80418b2597a46e0015317f8f4a93b910531
Chish <chishxd@gmail.com>
2024-10-16 19:16:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,35 +20,64 @@ balance = Balance()
 while True:
     newbal = balance.balance
     answer = setNum()
-    
     print("Welcome to Ring! Fellow Warrior")
     time.sleep(1)
     bet = int(input("Place your Bet! The Number should be between 1 - 10 only! "))
-    time.sleep(0)
-    amt = int(input("How Much money are you willing to pay? "))
-    if amt < newbal:
-        time.sleep(0.5)
-        print("Well said, Now")
-        time.sleep(1)
-        print("3")
-        time.sleep(1)
-        print("2")
-        time.sleep(1)
-        print("1")
-        time.sleep(1)
-        print(f"The Number is.... {answer}!!")
-        if bet == answer:
-            print("sattta jinklas ki bhava! Laav aani paishe")
-            newbal = balance.win_bal(amt)
+    if bet > 10:
+        bet = input("Please enter a valid bet: ")
+        amt = int(input("How Much money are you willing to pay? "))
+        if amt > newbal:
+            print("This Amount is not available on the account")
         else:
-            print("Marda, kay aasa. Ya parat")
-            newbal = balance.lose_bal(amt)
-        if newbal == 0:
-            addbal = int(input("Bhava Paishe smplet ki... Ghaal aani: "))
-            newbal = balance.add_bal(addbal)
-        print(newbal)
+            time.sleep(0.5)
+            print("Well said, Now")
+            time.sleep(1)
+            print("3")
+            time.sleep(1)
+            print("2")
+            time.sleep(1)
+            print("1")
+            time.sleep(1)
+            print(f"The Number is.... {answer}!!")
+            if bet == answer:
+                print(f"Contrats! You won {2*amt}")
+                newbal = balance.win_bal(amt)
+            else:
+                print("Ohh... You Lost! Better Luck Next Time!")
+                newbal = balance.lose_bal(amt)
+            if newbal == 0:
+                addbal = int(input("You are out of money, add more balance to you A/C: "))
+                newbal = balance.add_bal(addbal)
+            print(newbal)
     else:
-        print("This Amount is not available on the account")
-    e = input("Press E to exit: ").lower()
-    if e == "e":
-        break
+        amt = int(input("How Much money are you willing to pay? "))
+        if amt > newbal:
+            print("This Amount is not available on the account")
+        else:
+            time.sleep(0.5)
+            print("Well said, Now")
+            time.sleep(1)
+            print("3")
+            time.sleep(1)
+            print("2")
+            time.sleep(1)
+            print("1")
+            time.sleep(1)
+            print(f"The Number is.... {answer}!!")
+            if bet == answer:
+                print(f"Contrats! You won {2*amt}")
+                newbal = balance.win_bal(amt)
+            else:
+                print("Ohh... You Lost! Better Luck Next Time!")
+                newbal = balance.lose_bal(amt)
+            if newbal == 0:
+                addbal = int(input("You are out of money, add more balance to you A/C: "))
+                newbal = balance.add_bal(addbal)
+            
+        end = input('''Press 1/balance to check balance\nPress 2/end to exit: ''').lower().strip()
+        if end in ("1", "balance"):
+            print(f"Current Balance: {newbal}")
+        if end == "end":
+            print("See You Around!")
+            break
+        
