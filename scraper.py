172a376bbfefcc43a95177e7faf2ce7e089f99ca
Chinmay Shet <chishxd@gmail.com>
2025-03-14 20:44:58 +0530

    Self Made Scraper for USA sorted jobs

--- scraper.py
+++ scraper.py
@@ -3,25 +3,32 @@ from bs4 import BeautifulSoup
 
 print("Script started")
 
-URL = "https://realpython.github.io/fake-jobs/"
+URL = "https://www.python.org/jobs/"
 response = requests.get(URL, timeout=1)  # Add timeout
 
 soup = BeautifulSoup(response.content, "html.parser")
-results = soup.find(id = "ResultsContainer")
+results = soup.find("div", class_="row")
 
-job_cards = results.find_all("div", class_= "card-content")
+ol_list = results.find("ol", class_="list-recent-jobs list-row-container menu")
+li_list = ol_list.find_all("li")
 
-python_jobs = results.find_all("h2", string = lambda text: "python" in text.lower())
+usa_jobs = [
+    job_card for job_card in li_list if job_card.find("span", class_="listing-location") and "united states" in job_card.find("span", class_="listing-location").text.strip().lower()]
 
-python_job_cards = [h2_elements.parent.parent.parent for h2_elements in python_jobs]
-
-for job_card in python_job_cards:
-    title = job_card.find("h2", class_= "title")
-    company = job_card.find("h3", class_ = "company")
-    location = job_card.find("p", class_ = "location")
-    link_url = job_card.find_all('a')[1]["href"]
-    print(title.text.strip())
-    print(company.text.strip())
-    print(location.text.strip())
-    print(f"Apply Here: {link_url}\n")
-    print()
+for job_card in usa_jobs:
+    title_span = job_card.find("span", class_="listing-company-name")
+    title = title_span.find("a") if title_span else None
+    
+    company_span = job_card.find("span", class_="listing-company-name")
+    company = company_span.find("a") if company_span else None
+    
+    location_span = job_card.find("span", class_="listing-location")
+    location = location_span.find("a") if location_span else None
+    
+    link_url = title["href"] if title else None
+    
+    if title and company and location:
+        print(title.text.strip())
+        print(company.text.strip())
+        print(location.text.strip())
+        print(f"Apply Here: {link_url}\n")
