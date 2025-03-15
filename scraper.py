f85855268568138f1e0f6a6257b5ef56eb9e0da6
Chinmay Shet <chishxd@gmail.com>
2025-03-15 18:57:54 +0530

    Handled Exceptions and Improved speed

--- scraper.py
+++ scraper.py
@@ -1,34 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
 
-print("Script started")
+print("Script started\n")
 
 URL = "https://www.python.org/jobs/"
-response = requests.get(URL, timeout=1)  # Add timeout
+try:
+    response = requests.get(URL, timeout=1)  # Add timeout
+    response.raise_for_status()
+except requests.exceptions.RequestException as e:
+    print(f"Error: {e}")
+    exit()
 
 soup = BeautifulSoup(response.content, "html.parser")
 results = soup.find("div", class_="row")
 
-ol_list = results.find("ol", class_="list-recent-jobs list-row-container menu")
-li_list = ol_list.find_all("li")
+if not results:
+    print("Couldn't Find Any Jobs Now")
+    exit()
 
-usa_jobs = [
-    job_card for job_card in li_list if job_card.find("span", class_="listing-location") and "united states" in job_card.find("span", class_="listing-location").text.strip().lower()]
+jobs_list = results.find("ol", class_="list-recent-jobs list-row-container menu").find_all("li")
 
-for job_card in usa_jobs:
-    title_span = job_card.find("span", class_="listing-company-name")
-    title = title_span.find("a") if title_span else None
+
+# usa_jobs = [
+#     job_card for job_card in li_list if job_card.find("span", class_="listing-location") and "united states" in job_card.find("span", class_="listing-location").text.strip().lower()]
+
+for job_card in jobs_list:
+    title_span = job_card.find("span", class_="listing-company-name").find("a") if job_card.find("span", class_="listing-company-name").find("a") else None
+    # title = title_span.find("a") if title_span else None
     
-    company_span = job_card.find("span", class_="listing-company-name")
-    company = company_span.find("a") if company_span else None
+    company_span = job_card.find("span", class_="listing-company-name").find("a") if job_card.find("span", class_="listing-company-name").find("a") else None
+    # company = company_span.find("a") if company_span else None
     
-    location_span = job_card.find("span", class_="listing-location")
-    location = location_span.find("a") if location_span else None
+    location_span = job_card.find("span", class_="listing-location").find("a") if job_card.find("span", class_="listing-location").find("a") else None
+    # location = location_span.find("a") if location_span else None
     
-    link_url = title["href"] if title else None
+    link_url = title_span["href"] if title_span else None
     
-    if title and company and location:
-        print(title.text.strip())
-        print(company.text.strip())
-        print(location.text.strip())
-        print(f"Apply Here: {link_url}\n")
+    if not (title_span and company_span and location_span):
+        continue
+    location_text = location_span.text.strip().lower()
+
+    if "united states" not in location_text:
+        continue
+    print(title_span.text.strip())
+    print(company_span.text.strip())
+    print(location_span.text.strip())
+    print(f"Apply Here: {link_url}\n")
